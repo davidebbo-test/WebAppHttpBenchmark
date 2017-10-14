0d7bf80d9629ac05c880c8682f760e66a5fb5044
David Ebbo <david.ebbo@gmail.com>
2017-10-14 16:04:38 -0700

    Add test function

--- WebAppHttpBenchmark/Controllers/HomeController.cs
+++ WebAppHttpBenchmark/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +11,25 @@ namespace WebAppHttpBenchmark.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index(long AllocInMB = 0, int SleepInMS = 0, int LoopSpins = 0)
         {
-            ViewBag.Title = "Home Page";
+            long memory = AllocInMB;
+            int sleep = SleepInMS;
+            long iterations = LoopSpins;
+            var rnd = new Random();
 
-            return View();
+            // Allocate memory and fill it with random bytes
+            byte[] bytes = new byte[memory * 1024 * 1024];
+            rnd.NextBytes(bytes);
+
+            // Do an async sleep
+            await Task.Delay(sleep);
+
+            // Spin the CPU
+            for (long i = 0; i < iterations; i++) { }
+
+            Response.Headers.Add("X-server", Environment.GetEnvironmentVariable("COMPUTERNAME"));
+            return Json("Done", JsonRequestBehavior.AllowGet);
         }
     }
 }
